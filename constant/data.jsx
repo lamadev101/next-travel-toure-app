5a1226dabed19df59630d8a7a9b4c536b4f7c253
Deepika Timalsina <dixpikaa@gmail.com>
2023-05-16 21:30:33 +0545

    edited packages

--- constant/data.jsx
+++ constant/data.jsx
@@ -29,24 +29,24 @@ export const Navitems = [
 export const Packages = [
   {
     id: 12,
-    img:"https://images.pexels.com/photos/3873054/pexels-photo-3873054.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    title: "Bauddha Stupa",
-    duration: "2 Day & 1 Night",
-    price: "45$"
+    img:"https://images.unsplash.com/photo-1441321845576-f74e2c7a85fc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1748&q=80",
+    title: "Mountainside village",
+    duration: "6 Day & 5 Night",
+    price: "145$"
   },
   {
     id: 13,
-    img:"https://images.pexels.com/photos/14431158/pexels-photo-14431158.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    title: "Bindabasini Tample,Pokhara",
+    img:"https://images.unsplash.com/photo-1591029382569-f806703fcc37?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2676&q=80",
+    title: "Pokhara",
     duration: "3 Day & 2 Night",
     price: "56$"
   },
   {
     id: 14,
-    img:"https://images.pexels.com/photos/14367175/pexels-photo-14367175.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    title: "Patan Durbar Square",
-    duration: "1 Day",
-    price: "14$"
+    img:"https://images.unsplash.com/photo-1511215579272-6192432f83bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+    title: "Namche Bazar",
+    duration: "1 Week",
+    price: "289$"
   },
   {
     id: 16,
@@ -57,17 +57,17 @@ export const Packages = [
   },
   {
     id: 17,
-   img:"https://images.pexels.com/photos/11472639/pexels-photo-11472639.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    title: "PashupattiNath",
-    duration: "1 Day",
-    price: "10$"
+   img:"https://images.unsplash.com/photo-1474294101831-f103d3eb4d48?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+    title: "Machhapuchre BaseCamp",
+    duration: "4 Day 3 Night",
+    price: "105$"
   },
   {
     id: 18,
-    img:"https://images.pexels.com/photos/11757175/pexels-photo-11757175.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    title: "Bhaktapur Durbar Square",
-    duration: "1 Day ",
-    price: "27$"
+    img:"https://images.unsplash.com/photo-1485081669829-bacb8c7bb1f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80",
+    title: "Town of Hallstatt",
+    duration: "5 Day 4 Night ",
+    price: "214$"
   },
   {
     id: 19,
@@ -75,6 +75,13 @@ export const Packages = [
     title: "Lumbini",
     duration: "3 Day & 2 Night",
     price: "50$"
+  },
+  {
+    id: 20,
+    img:"https://images.unsplash.com/photo-1417052800325-788e0eaf22c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+    title: "tranquil ocean",
+    duration: "3 Day & 2 Night",
+    price: "59$"
   },
 ]
 
@@ -112,50 +119,85 @@ export const DestinationItems=[
 
 {
   id:1,
-  img:" https://images.unsplash.com/photo-1610997686651-98492fd08108?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1931&q=80",
+  img:" https://images.unsplash.com/photo-1618083840675-f5bdfd4a704e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
   place:"Pokhara"
   
 
 },
 {
   id:2,
-  img:" https://images.unsplash.com/photo-1610997686651-98492fd08108?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1931&q=80",
-  place:"Pokhara"
+  img:" https://images.unsplash.com/photo-1589550165428-79eb74318795?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80",
+  place:"Kathmandu"
   
 
 },
 {
   id:3,
-  img:" https://images.unsplash.com/photo-1610997686651-98492fd08108?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1931&q=80",
-  place:"Pokhara"
+  img:" https://images.unsplash.com/photo-1681635851078-57a587a6c2d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
+  place:"Kalinchowk"
   
 
 },
 {
   id:4,
-  img:" https://images.unsplash.com/photo-1610997686651-98492fd08108?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1931&q=80",
-  place:"Pokhara"
+  img:" https://images.unsplash.com/photo-1562462159-bc2c0c305fdb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2612&q=80",
+  place:"Chitwan"
   
 
 },
 {
   id:5,
-  img:" https://images.unsplash.com/photo-1610997686651-98492fd08108?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1931&q=80",
-  place:"Pokhara"
+  img:" https://images.unsplash.com/photo-1533005346585-b131e79eef3a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2748&q=80",
+  place:"Khumjung"
   
 
 },
 {
   id:6,
-  img:" https://images.unsplash.com/photo-1610997686651-98492fd08108?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1931&q=80",
-  place:"Pokhara"
+  img:" https://images.unsplash.com/photo-1630804597431-46a42500fe02?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+  place:"Rara Lake"
   
 
 },
 {
   id:7,
-  img:" https://images.unsplash.com/photo-1610997686651-98492fd08108?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1931&q=80",
-  place:"Pokhara"
+  img:" https://images.unsplash.com/photo-1603787663416-88d33de66699?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+  place:"Everest Base Camp"
+  
+
+},
+{
+  id:8,
+  img:" https://images.unsplash.com/photo-1668402786143-1c81c818e315?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+  place:"Annapurna Base Camp"
+  
+
+},
+{
+  id:9,
+  img:" https://images.unsplash.com/photo-1602102488252-c4c3daadf1c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1748&q=80",
+  place:"Ilam"
+  
+
+},
+{
+  id:10,
+  img:" https://images.unsplash.com/photo-1664215490963-a5cfd0c301dc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+  place:"Langtang"
+  
+
+},
+{
+  id:11,
+  img:" https://images.unsplash.com/photo-1592731057019-57ed336948ed?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
+  place:"Mustang"
+  
+
+},
+{
+  id:12,
+  img:" https://images.unsplash.com/photo-1668966780008-b7dae98f61f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+  place:"Namobuddha"
   
 
 },
